322d4111d35b1ce0c20da6ca61f6e93944e7ec9d
Joeeazy <joeeazy34@gmail.com>
2024-07-04 13:03:22 +0300

    final setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import "./App.css";
+import About from "./components/About.jsx";
+import Banner from "./components/Banner";
+import Community from "./components/Community";
+import Faq from "./components/Faq";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Join from "./components/Join";
+import Pricing from "./components/Pricing";
+import Workouts from "./components/Workouts";
 
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="max-w-[1440px] mx-auto bg-page overflow-hidden relative">
+      <Header />
+      <Banner />
+      <About />
+      <Workouts />
+      <Pricing />
+      <Community />
+      <Faq />
+      <Join />
+      <Footer />
+      {/* <div className='h-[4000px]'></div> */}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,48 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      primary: "Inter",
+      secondary: "Open Sans",
+    },
+    container: {
+      padding: {
+        DEFAULT: "0",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1038px",
+    },
+    extend: {
+      colors: {
+        primary: {
+          100: "#a5ffce",
+          200: "#4ae290",
+          300: "25c870",
+        },
+        neutral: {
+          100: "#ffffff",
+          200: "#dedee3",
+          300: "#9797a1",
+          400: "#595962",
+          500: "#131316",
+        },
+        page: "#fcfcff",
+      },
+      backgroundImage: {
+        banner: "url('/public/img/banner/bg.png')",
+        faq: "url('/public/img/faq/bg.svg')",
+      },
+      backgroundSize: {
+        auto: "auto",
+        cover: "cover",
+        contain: "contain",
+        custom: "95%",
+      },
+    },
   },
   plugins: [],
 };
