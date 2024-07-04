fd9aa6d18b40f068a3542b0b87c4711ce7510d88
Joeeazy <joeeazy34@gmail.com>
2024-07-04 13:59:49 +0300

    Banner section done

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,15 @@ import Join from "./components/Join";
 import Pricing from "./components/Pricing";
 import Workouts from "./components/Workouts";
 
+// import aos
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+  Aos.init({
+    duration: 2500,
+    delay: 400,
+  });
   return (
     <div className="max-w-[1440px] mx-auto bg-page overflow-hidden relative">
       <Header />
