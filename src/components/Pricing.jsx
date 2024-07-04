7b1d075cf89c224be4e4c6b2076cba153e3f9716
Joeeazy <joeeazy34@gmail.com>
2024-07-04 20:42:48 +0300

    pricing section complete

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
+//import contents
+import { pricing } from "../constants";
+
+//import component
+import PlanList from "./PlanList";
+
 export default function Pricing() {
-  return <div>Pricing</div>;
+  //destructre constants
+  const { icon, title, plans } = pricing;
+  return (
+    <section className="section">
+      {/* section title */}
+      <div
+        className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0"
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-delay="200"
+      >
+        <img src={icon} alt="Pricing icon" />
+        <h2 className="h2">
+          {title}
+          <span className="text-primary-200">.</span>
+        </h2>
+      </div>
+      {/* PlanList component */}
+      <PlanList plans={plans} />
+    </section>
+  );
 }
